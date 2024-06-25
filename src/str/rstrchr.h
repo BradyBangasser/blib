b49a8433566ace97c251db12100006595657f41f
Brady Bangasser <70297830+BradyBangasser@users.noreply.github.com>
2024-06-24 19:45:56 -0500

    Update and rename rmemshr.h to rstrchr.h and implemented it

---
+++ src/str/rstrchr.h
@@ -0,0 +1,11 @@
+#include <string.h>
+
+/**
+ * strchr but it looks for the last instance of a character
+*/
+
+static inline char *rstrchr(const char *str, char c) {
+  int i = strlen(str);
+  while (i-- > 0) if (str[i] == c) return str + i;
+  return NULL;
+}
