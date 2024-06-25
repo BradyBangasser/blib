47eb34717cb2836d0cc509cd09079aaac4969c19
Brady Bangasser <70297830+BradyBangasser@users.noreply.github.com>
2024-06-24 20:12:21 -0500

    Implemented rmkdir

--- src/fs/rmkdir.h
+++ src/fs/rmkdir.h
@@ -1,9 +1,36 @@
+#pragma once
+
 #include <sys/stat.h>
+#include "str/rstrcmp.h"
 
 /**
  *
  *
 /*
-static inline int rmkdir(const char *path) {
-  
+static int rmkdir(const char *path, mode_t mode) {
+  char *cpath, *lastSlash;
+  int res;
+
+  if (path[0] == 0) return -1;
+
+  if ((res = mkdir(path, mode)) == 0) {
+    return 0;
+  }
+
+  if (errno == 2) {
+    // Either permissions are blocking the creation or it has to create the parent dirtectories first
+
+    if ((lastSlash = rstrchr(path, '/')) == NULL) {
+      return -1;
+    }
+
+    cpath = strndup(path, (unsigned long long) lastSlash - (unsigned long long) path - 1);
+    res = rmkdir(cpath, mode);
+
+    if (res == -1) {
+      return -1;
+    }
+
+    return mkdir(path, mode);
+  }
 }
